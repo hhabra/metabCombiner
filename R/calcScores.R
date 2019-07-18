948ca13f459bedcf6ec26976203bbaded6186f7a
Habra <hhani@umhs.med.umich.edu>
2019-07-18 12:34:42 -0400

    fixed issue with duplicate columns

--- R/calcScores.R
+++ R/calcScores.R
@@ -1,26 +1,26 @@
-##mzdiff
+## calculate differences in m/z between complementary features
 #'
+#' @param rows integer vector. Indices to calculate m/z differences.
 #'
+#' @param mzx  numeric vector. m/z values from dataset X.
 #'
-#'
-#'
-#'
+#' @param mzy  numeric vector. m/z values from dataset Y.
+#' 
+#' @param usePPM logical. Option to calculate relative differences (ppm).
+#' 
+#' @return Vector of numeric relative or absolute m/z differences.
 ##
 mzdiff <- function(rows, mzx, mzy, usePPM){
     diffs = abs(mzx[rows] - mzy[rows])
   
     if(usePPM)
-        diffs = 1e6 * diffs / sapply(rows, function(row) min(mzx[row], mzy[row])) 
+        diffs = 1e6 * diffs/ sapply(rows, function(r) min(mzx[r], mzy[r])) 
     
     return(diffs)
 }
 
-
-
-
-
+##
 #'
-#'@description used to compare ID or adduct strings
 #'
 #'
 #'
@@ -40,11 +40,12 @@ compare_strings <- function(string){
 
 #' Calculate the pairwise score between two grouped features
 #' 
-#' @param A Numeric Parameter penalizing m/z differences
+#' @param A Numeric. Weight for m/z differences.
 #' 
-#' @param B Numeric Parameter penalizing projected and observed retention times
+#' @param B Numeric. Weight for differences btw. fitted & observed 
+#' retention times
 #' 
-#' @param G Numeric Paramater penalizing Q differences
+#' @param G Numeric. Weight for differences in Q (relative abundance).
 #'   
 #' @param mzdiff Difference between feature m/z values; can be absolute or
 #'               relative (ppm)
@@ -57,11 +58,12 @@ compare_strings <- function(string){
 #' @param adductPenalty Numeric. If useAdduct set to True, used to divide total score
 #'                      if adduct labels are mismatched.
 #' 
-#' @return Pairwise score between two grouped features(between 0 & 1) evaluating the 
-#'         likelihood of a compound match.
+#' @return Pairwise score between two grouped features(between 0 & 1) evaluating
+#'         the likelihood of a compound match.
 #' 
 #' @examples 
-#' scorePair(A = 100, B = 5, C = 0.4, mzdiff = 0.003, rtdiff = 0.5, qdiff = 0.1, rtrange = 20, adductMatch = TRUE, adductPenalty = 1)
+#' scorePair(A = 100, B = 5, C = 0.4, mzdiff = 0.003, rtdiff = 0.5, qdiff = 0.1,
+#' rtrange = 20, adductMatch = FALSE, adductPenalty = 1)
 #' 
 scorePairs <- function(A, B, C, mzdiff, rtdiff, qdiff, rtrange, usePPM, 
                        adductMatch, adductPenalty){
@@ -73,14 +75,17 @@ scorePairs <- function(A, B, C, mzdiff, rtdiff, qdiff, rtrange, usePPM,
     return(score)
 }
 
-scorePairs(A = 100, B = 5, C = 0.4, rtrange = 15, mzdiff = abs(cT$mzx - cT$mzy),
-           rtdiff = abs(cT$rty - cT$rtProj), qdiff = abs(cT$Qx - cT$Qy),
-           adductPenalty = 2)
 
-
-##calcScores
+## Assign pairwise confidence scores between all grouped metabolomics features.
 #'
-#'
+#' @param object  metabCombiner object.
+#' 
+#' @param A Numeric. Weight for m/z differences.
+#' 
+#' @param B Numeric. Weight for differences btw. fitted & observed 
+#' retention times
+#' 
+#' @param G Numeric. Weight for differences in Q (relative abundance).
 #'
 #'
 ##
@@ -96,8 +101,11 @@ calcScores <- function(object, A, B, C, fit = c("loess", "gam"), usePPM = FALSE,
     if(A < 0 | B < 0 | C < 0)
         stop("parameters 'A', 'B', 'C' must be non-negative")
   
-    cTab = combinerTable(object)
-  
+    cTable = combinerTable(object)
+    
+    if(nrow(cTable) == 0)
+        stop("missing Combiner table")
+    
     fit = match.arg(fit)
     model = getModel(object, fit = fit)
     
@@ -111,34 +119,43 @@ calcScores <- function(object, A, B, C, fit = c("loess", "gam"), usePPM = FALSE,
   
     #handling groups parameter
     if(is.null(groups))
-        groups = 1:max(cTab$group)
+        groups = 1:max(cTable["group"])
   
-    if(any(!groups %in% cTab$group))
+    if(any(!groups %in% cTable$group))
         stop("Invalid parameter 'groups'")
   
     #rows whose scores are computed according to groups
-    rows = which(cTab$group %in% groups) 
+    rows = which(cTable$group %in% groups) 
     
-    cTab$rtProj[rows] = predict(model, newdata = cTab$rtx[rows])
+    cTable$rtProj[rows] = predict(model, newdata = cTable[rows,])
     
-    cTab$score[rows] = scorePairs(
-                        A = A, B = B, C = C,
-                        mzdiff = mzdiff(rows, cTab$mzx, cTab$mzy, usePPM),
-                        rtdiff = abs(cTab$rty[rows] - cTab$rtProj[rows]),
-                        qdiff = abs(cTab$Qx[rows] - cTab$Qy[rows]),
-                        rtrange = rtrange,
-                        usePPM = usePPM,
-                        adductMatch = adductMatch,
-                        adductPenalty = adductPenalty
-    ) %>% round(digits = 4)
+    cTable$score[rows] = scorePairs(
+                         A = A, B = B, C = C,
+                         mzdiff = mzdiff(rows, cTable$mzx, cTable$mzy, usePPM),
+                         rtdiff = abs(cTable$rty[rows] - cTable$rtProj[rows]),
+                         qdiff = abs(cTable$Qx[rows] - cTable$Qy[rows]),
+                         rtrange = rtrange,
+                         usePPM = usePPM,
+                         adductMatch = adductMatch,
+                         adductPenalty = adductPenalty
+    )
+    
+    cTable[c("rtProj", "score")] = round(cTable[c("rtProj", "score")], 4)
+    
+    
+    ##bug fix for duplicated column names
+    cT = cTable[ , !duplicated(colnames(cTable))]
 
-    cTab[rows,] = cTab[rows,] %>% group_by(mzx, rtx) %>% 
-                    mutate(rankX_Y = dense_rank(desc(score))) %>%
-                    ungroup() %>% group_by(mzy, rty) %>% 
-                    mutate(rankY_X= dense_rank(desc(score))) %>%
-                    ungroup()
+    ##calculate score ranking of features 
+    cT[rows,] = cT[rows,] %>% group_by(mzx, rtx) %>% 
+                  mutate(rankX_Y = dense_rank(desc(score))) %>%
+                  ungroup() %>% group_by(mzy, rty) %>% 
+                  mutate(rankY_X= dense_rank(desc(score))) %>%
+                  ungroup()
     
-    object@combinerTable = arrange(cTab, group, desc(score)) 
+    cTable[c("rankX_Y", "rankY_X")] = cT[c("rankX_Y", "rankY_X")]
+
+    object@combinerTable = cTable[with(cTable, order(group, desc(score))), ]
     
     return(object)
 }
