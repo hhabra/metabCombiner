3c2c77621bac973464b8c8ec3f41bbabf7578590
Hani Habra <hhabra1@gmail.com>
2020-05-09 19:10:40 -0400

    Dataset Descriptions

---
+++ R/data.R
@@ -0,0 +1,42 @@
+#' @title 30 minute Metabolomics Analysis of Human Plasma
+#'
+#' An example metabolomics dataset consisting of human plasma from Red Cross
+#' and CHEAR cohorts, plus pooled aliquots and negative controls, acquired
+#' with a 30 minute total Reversed-Phase Liquid Chromatography and a QTOF
+#' instrument in the positive ionization mode.
+#'
+#' It is data frame with 8286 rows and 22 variables, including:
+#' \itemize{
+#'   \item{feature}{generic feature identifiers}
+#'   \item{identity}{identities of feature (if known); feature identifier (if
+#'                    unknown); names in parantheses if ambiguous)}
+#'   \item{adduct}{adduct, fragment, or multimer annotations; labels in
+#'                 parantheses if ambiguous}
+#'   \item{mz}{m/z values of metabolomics features}
+#'   \item{rt}{rt values of metabolomics features}
+#'   \item{...}{column names for CHEAR, Red Cross, Pooled, and Blank samples}
+#' }
+"plasma30"
+
+
+#' @title 20 minute Metabolomics Analysis of Human Plasma
+#'
+#' An example metabolomics dataset consisting of human plasma from Red Cross
+#' and CHEAR cohorts, plus pooled aliquots and negative controls, acquired
+#' with a 20 minute total Reversed-Phase Liquid Chromatography & QTOF-MS
+#' instrument in the positive ionization mode.
+#'
+#'  It is a data frame with 8910 rows and 22 variables, including:
+#' \itemize{
+#'   \item{feature}{generic feature identifiers}
+#'   \item{identity}{identities of feature (if known); feature identifier (if
+#'                    unknown); names in parantheses if ambiguous)}
+#'   \item{adduct}{adduct, fragment, or multimer annotations; labels in
+#'                 parantheses if ambiguous}
+#'   \item{mz}{m/z values of metabolomics features}
+#'   \item{rt}{rt values of metabolomics features}
+#'   \item{...}{column names for CHEAR, Red Cross, Pooled, and Blank samples}
+#' }
+"plasma20"
+
+
