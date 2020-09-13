45664081657a9520f420ff8afb23e56385b3b6cc
Hani Habra <hhabra1@gmail.com>
2020-09-12 23:20:18 -0400

    updated documentation

--- R/plot_fit.R
+++ R/plot_fit.R
@@ -19,33 +19,28 @@
 #'
 #' @param ... Other variables passed into graphics::plot
 #'
+#' @return no values returned
+#'
 #' @examples
-#' \dontrun{
-#' library(metabCombiner)
 #' data(plasma30)
 #' data(plasma20)
 #'
 #' p30 <- metabData(plasma30, samples = "CHEAR")
 #' p20 <- metabData(plasma20, samples = "Red", rtmax = 17.25)
-#' p.combined = metabCombiner(xdata = p30, ydata = p20, binGap = 0.0075)
-#' p.combined = selectAnchors(p.combined, tolMZ = 0.003, tolQ = 0.3, windY = 0.02)
-#' p.combined = fit_gam(p.combined, k = seq(12,20,2), iterFilter = 1)
-#' p.combined = fit_loess(p.combined, spans = seq(0.2,0.3, 0.02), iterFilter = 2)
+#' p.comb = metabCombiner(xdata = p30, ydata = p20, binGap = 0.0075)
+#' p.comb = selectAnchors(p.comb, tolmz = 0.003, tolQ = 0.3, windy = 0.02)
+#' p.comb = fit_gam(p.comb, k = 20, iterFilter = 1)
 #'
 #' ##plot of GAM fit
-#' plot(p.combined, main = "Example GAM Fit Plot", xlab = "X Dataset RTs",
-#'      ylab = "Y Dataset RTs", pcol = "red", lcol = "blue", lwd = 5, pch = 19)
+#' plot(p.comb, main = "Example GAM Fit Plot", xlab = "X Dataset RTs",
+#'      ylab = "Y Dataset RTs", pcol = "red", lcol = "blue", lwd = 5,
+#'      fit = "gam", pch = 19, remove.outliers = TRUE)
 #'
 #' grid(lwd =  2, lty = 3 ) #adding gridlines
 #'
-#' ##plot of loess fit
-#' plot(p.combined, fit = "loess", main = "Example loess Fit Plot",
-#'      xlab = "X Dataset RTs", ylab = "Y Dataset RTs", pcol = "dark orange",
-#'      lcol = "dark green", pch = 8, remove.outliers = TRUE)
-#'}
 #' @export
 plot_fit <- function(object, fit = c("gam","loess"), pcol, lcol, lwd,
-                     remove.outliers = FALSE, ...)
+                    remove.outliers = FALSE, ...)
 {
     fit = match.arg(fit)
     model = getModel(object, fit = fit)
@@ -54,16 +49,14 @@ plot_fit <- function(object, fit = c("gam","loess"), pcol, lcol, lwd,
         stop(paste("object missing model of type", fit))
 
     if(fit == "loess")
-        data = data.frame(rtx = model[["x"]],
-                          rty = model[["y"]],
-                          pred = model[["fitted"]],
-                          weights = model[["weights"]])
+        data = data.frame(rtx = model[["x"]], rty = model[["y"]],
+                            pred = model[["fitted"]],
+                            weights = model[["weights"]])
 
     else if (fit == "gam")
-        data = data.frame(rtx = model$model$rtx,
-                          rty = model$model$rty,
-                          preds = model[["fitted.values"]],
-                          weights = model[["prior.weights"]])
+        data = data.frame(rtx = model$model$rtx, rty = model$model$rty,
+                            preds = model[["fitted.values"]],
+                            weights = model[["prior.weights"]])
 
     data = dplyr::arrange(data, .data$rtx)
 
