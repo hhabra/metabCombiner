985543e8bb4e5f97039df07636bf30f41f26379b
Hani Habra <hhabra1@gmail.com>
2020-09-12 23:16:36 -0400

    shortened function with par_check

--- R/labelRows.R
+++ R/labelRows.R
@@ -1,12 +1,51 @@
+
+#' Reform Report with New Labels
+#'
+#' @description Helper function for labelRows(). After determining row
+#' annotations, stitches together the metadata, the row annotations, and
+#' the sample + extra value data.
+#'
+#' @param fields data.frame abridged combinedTable with metadata fields.
+#'
+#' @param values data.frame combinedTable with sample (+ extra) columns
+#'
+#' @noRd
+formLabeledTable <- function(fields, values, remove)
+{
+    #option to eliminate rows labeled as removables
+    if(remove == TRUE){
+        keepRows = which(fields[["labels"]] != "REMOVE")
+        fields = fields[keepRows,]
+        values = values[keepRows,]
+    }
+
+    #when there are existing columns named "labels", "subgroup", "alt"
+    labnames = c("labels", "subgroup", "alt")
+    N = 1
+
+    while(any(names(values) %in% labnames)){
+        labnames = paste(c("labels", "subgroup", "alt"), N, sep = ".")
+        names(fields)[seq(16,18)] = labnames
+        N = N+1
+    }
+
+    cTable = data.frame(fields, values, stringsAsFactors = FALSE,
+                        check.names = FALSE)
+
+    return(cTable)
+
+}
+
+
 #' @title Annotate and Remove Report Rows
 #'
 #' @description
-#' Automated method for determining identity-matching, removable, and conflicting
-#' feature pair alignments (FPAs) in \code{combinedTable}. FPAs that may require
-#' further inspection are labeled "CONFLICT" and organized into subgroups if they
-#' fall within some small measure (in score or mz/rt) of the top-ranked FPA.
-#'
-#' @param object  Either a \code{metabCombiner} object or \code{combinedTable}.
+#' Method for annotation of identity-matched, removable, & conflicting feature
+#' pair alignments (FPAs) in \code{combinedTable}. FPAs that fall within some
+#' small measure (in score or mz/rt) of the top-ranked FPA may require further
+#' inspection are organized into subgroups.
+#' .
+#' @param object Either a \code{metabCombiner} object or \code{combinedTable}.
 #'
 #' @param maxRankX  Integer. Maximum allowable rank for X dataset features.
 #'
@@ -15,24 +54,23 @@
 #' @param minScore  Numeric. Minimum allowable score (between 0 & 1) for
 #'                  metabolomics FPAs.
 #'
-#' @param method  Conflict detection method. If equal to "score" (default method),
-#'                assigns a conflict subgroup if score of lower-ranking FPA is
-#'                within some tolerance of higher-ranking FPA. If set to "mzrt",
-#'                assigns a conflicting subgroup if within a small m/z & rt
-#'                distance of the top-ranked FPA.
+#' @param method Conflict detection method. If equal to "score" (default),
+#'              assigns a conflict subgroup if score of lower-ranking FPA is
+#'              within some tolerance of higher-ranking FPA. If set to "mzrt",
+#'              assigns a conflicting subgroup if within a small m/z & rt
+#'              distance of the top-ranked FPA.
 #'
-#' @param conflict Numeric. If method = "score", a constant (between 0 & 1)
-#'                 score difference between a pair of conflicting FPAs.
-#'                 If method = "mzrt", a length 4 numeric: (m/z, rt, m/z, rt)
-#'                 tolerances, the first pair for X dataset features and the
-#'                 second pair for Y dataset features.
+#' @param conflict  numeric used to determine subgroups. If method = "score", a
+#'                  constant (between 0 & 1) score difference between a pair of
+#'                  conflicting FPAs. If method = "mzrt", a length 4 numeric:
+#'                  (m/z, rt, m/z, rt) tolerances, the first pair for X dataset
+#'                  features and the second pair for Y dataset features.
 #'
-#' @param balanced  Logical. Optional processing of "balanced" groups, defined as
-#'                  groups with equal numbers of features from input datasets where
-#'                  all features have a 1-1 match.
+#' @param balanced  Logical. Optional processing of "balanced" groups, defined
+#'                  as groups with an equal number of features from input
+#'                  datasets where all features have a 1-1 match.
 #'
-#' @param remove  Logical. Option to keep or discard rows deemed removable (labeled
-#'                "REMOVE" in \code{combinedTable})
+#' @param remove  Logical. Option to keep or discard rows deemed removable.
 #'
 #' @param brackets_ignore character. Bracketed identity strings of the types
 #' in this argument will be ignored
@@ -64,145 +102,77 @@
 #'    \code{conflict} thresholds: labeled "REMOVE". Otherwise,
 #'
 #' @return  updated \code{combinedTable} or \code{metabCombiner} object. The
-#' tabel will have three new columns:
+#' table will have three new columns:
 #'
 #' \item{labels}{characterization of feature alignments as described}
 #' \item{subgroup}{conflicting subgroup number of feature alignments}
-#' \item{alt}{alternative subgroup for rows conflicting with multiple top-matches}
+#' \item{alt}{alternate subgroup for rows in multiple feature pair conflicts}
 #'
 #' @examples
-#' \dontrun{
-#' library(metabCombiner)
 #' data(plasma30)
 #' data(plasma20)
 #'
 #' p30 <- metabData(plasma30, samples = "CHEAR")
 #' p20 <- metabData(plasma20, samples = "Red", rtmax = 17.25)
-#' p.combined = metabCombiner(xdata = p30, ydata = p20, binGap = 0.0075)
-#' p.combined = selectAnchors(p.combined, tolMZ = 0.003, tolQ = 0.3, windY = 0.02)
-#' p.combined = fit_gam(p.combined, k = seq(12,20,2), iterFilter = 1)
-#' p.combined = calcScores(p.combined, A = 90, B = 14, C = 0.5)
-#' cTable = combinedTable(p.combined)
+#' p.comb = metabCombiner(xdata = p30, ydata = p20, binGap = 0.0075)
+#' p.comb = selectAnchors(p.comb, tolmz = 0.003, tolQ = 0.3, windy = 0.02)
+#' p.comb = fit_gam(p.comb, k = 20, iterFilter = 1)
+#' p.comb = calcScores(p.comb, A = 90, B = 14, C = 0.5)
+#' cTable = combinedTable(p.comb)
 #'
 #' ##example using score-based conflict detection method
-#' lTable = labelRows(cTable, method = "score", conflict = 0.2)
+#' lTable = labelRows(cTable, maxRankX = 3, maxRankY = 2, minScore = 0.5,
+#'     method = "score", conflict = 0.2)
 #'
 #' ##example using mzrt-based conflict detection method
-#' lTable = labelRows(cTable, method = "mzrt", conflict = c(0.005, 1, 0.005,0.5))
+#' lTable = labelRows(cTable, method = "mzrt", maxRankX = 3, maxRankY = 2,
+#'                      conflict = c(0.005, 1, 0.005,0.5))
 #'
-#' ##example changing thresholds for rank and minimum score
-#' lTable = labelRows(cTable, minScore = 0.5, maxRankX = 2, maxRankY = 2,
-#'                    method = "score", conflict = 0.1)
-#' }
 #' @export
 labelRows <- function(object, maxRankX = 3, maxRankY = 3, minScore = 0.3,
-                      conflict, method = c("score", "mzrt"), balanced = TRUE,
-                      remove = FALSE, brackets_ignore = c("(", "[", "{"))
+                        conflict, method = c("score", "mzrt"), balanced = TRUE,
+                        remove = FALSE, brackets_ignore = c("(", "[", "{"))
 {
-    if(iscombinedTable(object) == 0)
-        cTable = object
-
+    if(isCombinedTable(object) == 0) cTable = object
+    else if(isMetabCombiner(object) == 0) cTable = combinedTable(object)
     else{
-        code = isMetabCombiner(object)
-
-        if(code)
-            stop(combinerError(code, "metabCombiner"))
-
-        cTable = combinedTable(object)
+        combinerCheck(isMetabCombiner(object), "metabCombiner", "warning")
+        combinerCheck(isCombinedTable(object), "combinedTable", "warning")
+        stop("input object is not a valid metabCombiner or combinedTable")
     }
-
-    if(!class(maxRankX) %in% c("numeric", "integer") |
-      !class(maxRankY) %in% c("numeric", "integer"))
-        stop("arguments maxRankX & maxRankY must be integers")
-
+    method = match.arg(method)
+    check_lblrows_pars(maxRankX, maxRankY, minScore,balanced, method, conflict)
     maxRankX = as.integer(maxRankX[1])
     maxRankY = as.integer(maxRankY[1])
-
-    if(maxRankX < 1 | maxRankY < 1)
-        stop("arguments maxRankX & maxRankY must be greater than 1")
-
-    if(class(minScore) != "numeric" | minScore > 1 | minScore < 0)
-        stop("argument 'minScore' must be a numeric value between 0 and 1")
-
-    if(!is.logical(balanced))
-        stop("expected a logical for argument 'balanced'")
-
-    method = match.arg(method)
-
-    if(method == "score"){
-        if(!is.numeric(conflict) | conflict > 1 | conflict < 0)
-            stop("argument 'conflict' must be a numeric value between 0 & 1")
-
-        if(length(conflict) > 1)
-            stop("with method = score, constant value expected for conflict")
-    }
-
-    else if(method == "mzrt"){
-        if(length(conflict) != 4)
-            stop("with method = mzrt, length 4 vector expected for conflict)")
-
-        if(any(conflict < 0))
-            stop("values in 'conflict' argument must be non-negative")
-    }
+    minScore = as.numeric(minScore[1])
 
     cTable = cTable[with(cTable, order(`group`, desc(`score`))), ]
-
-    values = cTable[,-c(1:15)]
-    fields = cTable[,1:15]
+    values = cTable[,-seq(1,15)]
+    fields = cTable[,seq(1,15)]
     rm(cTable)
 
     fields[["labels"]] = compare_strings(fields[["idx"]], fields[["idy"]],
-                                       "IDENTITY", "", brackets_ignore)
-
+                                        "IDENTITY", "", brackets_ignore)
     fields[["subgroup"]] = integer(nrow(fields))
     fields[["alt"]] = integer(nrow(fields))
-
-    #score: 1,  mzrt: 2
-    method = as.integer(ifelse(method == "score", 1, 2))
+    method = as.integer(ifelse(method == "score", 1, 2)) #score: 1,  mzrt: 2
 
     fields[["labels"]] = .Call("labelRows",
-                                labels = fields[["labels"]],
-                                subgroup = fields[["subgroup"]],
-                                alt = fields[["alt"]],
-                                mzx = fields[["mzx"]],
-                                mzy = fields[["mzy"]],
-                                rtx = fields[["rtx"]],
-                                rty = fields[["rty"]],
-                                score = fields[["score"]],
-                                rankX = fields[["rankX"]],
-                                rankY = fields[["rankY"]],
-                                group = fields[["group"]],
-                                balanced = balanced,
-                                conflict = conflict,
-                                minScore = minScore,
-                                maxRankX = maxRankX,
-                                maxRankY = maxRankY,
-                                method = method,
-                                PACKAGE = "metabCombiner")
+                            labels = fields$labels,
+                            subgroup = fields$subgroup,
+                            alt = fields$alt, mzx = fields$mzx,
+                            mzy = fields$mzy, rtx = fields$rtx,
+                            rty = fields$rty, score = fields$score,
+                            rankX = fields$rankX, rankY = fields$rankY,
+                            group = fields$group, balanced = balanced,
+                            conflict = conflict, minScore = minScore,
+                            maxRankX = maxRankX, maxRankY = maxRankY,
+                            method = method, PACKAGE = "metabCombiner")
 
-      #option to eliminate rows labeled as removables
-      if(remove == TRUE){
-          keepRows = which(fields[["labels"]] != "REMOVE")
-          fields = fields[keepRows,]
-          values = values[keepRows,]
-      }
+    cTable = formLabeledTable(fields, values, remove)
 
-    #when there are existing columns named "labels", "subgroup", "alt"
-    labnames = c("labels", "subgroup", "alt")
-    N = 1
-
-    while(any(names(values) %in% labnames)){
-        labnames = paste(c("labels", "subgroup", "alt"), N, sep = ".")
-        names(fields)[16:18] = labnames
-        N = N+1
-    }
-
-    cTable = data.frame(fields, values, stringsAsFactors = FALSE,
-                        check.names = FALSE)
-
-    if(class(object) == "metabCombiner")
+    if(methods::is(object, "metabCombiner"))
         object@combinedTable = cTable
-
     else
         object = cTable
 
