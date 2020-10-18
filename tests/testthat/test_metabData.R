e3ae6a5e6f0a1d31cdc50736f41b17f75d08bd95
Hani Habra <hhabra1@gmail.com>
2020-10-17 20:21:19 -0400

    changes to metabData filter_missingness test, no empty datasets allowed

--- tests/testthat/test_metabData.R
+++ tests/testthat/test_metabData.R
@@ -9,41 +9,41 @@ test_that("field detection tests",{
 
     p20 = metabData(plasma20, samples = "Red", extra = "CHEAR")
 
-    expect_length(getSamples(p20), 5)
-    expect_length(getExtra(p20), 5)
-    expect_false(any(getSamples(p20) %in% getExtra(p20)))
+    testthat::expect_length(getSamples(p20), 5)
+    testthat::expect_length(getExtra(p20), 5)
+    testthat::expect_false(any(getSamples(p20) %in% getExtra(p20)))
 
     data = getData(p20)
-    expect_equal(nrow(plasma20), nrow(data))
+    testthat::expect_equal(nrow(plasma20), nrow(data))
 
     p20_2 = metabData(plasma20, samples = "RedCross", extra = "CH")
-    expect_equivalent(p20, p20_2)
+    testthat::expect_equivalent(p20, p20_2)
 
     plasma20$rt[25] = plasma20$rt[25] * -1
-    expect_error(metabData(plasma20, samples = "RedCross", extra = "CHEAR"))
+    testthat::expect_error(metabData(plasma20, samples = "RedCross", extra = "CHEAR"))
 
     plasma20$rt[25] = plasma20$rt[25] * -10000
-    expect_warning(metabData(plasma20, samples = "RedCross", extra = "CHEAR"))
+    testthat::expect_warning(metabData(plasma20, samples = "RedCross", extra = "CHEAR"))
 
     plasma20$rt[25] = plasma20$rt[25] * 1/10000
     plasma20$mz[3] = plasma20$mz[3] * 100
-    expect_warning(metabData(plasma20, samples = "RedCross", extra = "CHEAR"))
+    testthat::expect_warning(metabData(plasma20, samples = "RedCross", extra = "CHEAR"))
 
     plasma20$mz[3] = plasma20$mz[3] / 100
     p20 = suppressWarnings(metabData(plasma20))
-    expect_equivalent(grep("POOL|CHEAR|Red|Bl", names(plasma20), value = TRUE),
+    testthat::expect_equivalent(grep("POOL|CHEAR|Red|Bl", names(plasma20), value = TRUE),
                       getSamples(p20))
 
-    expect_error(metabData(plasma20, mz = "mz", rt = "mz"))
-    expect_error(metabData(plasma20, rt = "id"))
+    testthat::expect_error(metabData(plasma20, mz = "mz", rt = "mz"))
+    testthat::expect_error(metabData(plasma20, rt = "id"))
 })
 
 test_that("RT filtering test", {
     data("plasma20")
     p20 = metabData(plasma20, samples = "CHEAR", misspc = 15, rtmax = 17.25)
 
-    expect_lt(max(getData(p20)[["rt"]]), 17.25)
-    expect_gt(getStats(p20)[["filtered_by_rt"]], 0)
+    testthat::expect_lt(max(getData(p20)[["rt"]]), 17.25)
+    testthat::expect_gt(getStats(p20)[["filtered_by_rt"]], 0)
 })
 
 
@@ -52,11 +52,10 @@ test_that("filter_missingness test", {
 
     plasma20[["CHEAR.20min.2"]] = NA
     plasma20[["CHEAR.20min.2"]][100] = 1000
-    plasma20[["CHEAR.20min.3"]][100] = NA
 
     p20 = metabData(plasma20, samples = "CHEAR", misspc = 15)
 
-    expect_equal(getStats(p20)[["final_count"]], 0)
+    testthat::expect_equal(getStats(p20)[["final_count"]], 1)
 
 })
 
@@ -66,7 +65,7 @@ test_that("filter_duplicates test", {
     s = seq(1001.1,1.1, -1)
     data = data.frame(mz = mz, rt = rt, s = s)
     d = suppressWarnings(metabData(data, duplicate = c(0.01,0.01)))
-    expect_equal(getStats(d)[["filtered_as_duplicates"]], 500)
+    testthat::expect_equal(getStats(d)[["filtered_as_duplicates"]], 500)
 })
 
 
