7e6ea4533a29a5d64d76780b6c93315d500c7b07
Hani Habra <hhabra1@gmail.com>
2021-05-19 00:05:47 -0400

    concatenation with featdata enabled

--- R/write2file.R
+++ R/write2file.R
@@ -24,20 +24,22 @@
 #' p.comb <- selectAnchors(p.comb, tolmz = 0.003, tolrtq = 0.3, windy = 0.02)
 #' p.comb <- fit_gam(p.comb, k = 20, iterFilter = 1)
 #' p.comb <- calcScores(p.comb, A = 90, B = 14, C = 0.5)
+#' p.comb <- labelRows(p.comb, maxRankX = 2, maxRankY = 2, remove = TRUE)
 #'
 #' \donttest{
 #' ###using metabCombiner object as input
 #' write2file(p.comb, file = "plasma-combined.csv", sep = ",")
 #'
-#' ###using combinedTable report as input
-#' cTable <- combinedTable(p.comb)
+#' ###using combinedTable report and feature data as input
+#' cTable <- cbind.data.frame(combinedTable(p.comb), featdata(p.comb))
 #' write2file(cTable, file = "plasma-combined.txt", sep = "\t")
 #' }
 #' @export
 ##
 write2file <- function(object, file, sep = ","){
     if(isCombinedTable(object) == 0) cTable = object
-    else if(isMetabCombiner(object) == 0) cTable = combinedTable(object)
+    else if(isMetabCombiner(object) == 0)
+        cTable = cbind.data.frame(combinedTable(object), featdata(object))
     else{
         combinerCheck(isMetabCombiner(object), "metabCombiner", "warning")
         combinerCheck(isCombinedTable(object), "combinedTable", "warning")
@@ -47,8 +49,8 @@ write2file <- function(object, file, sep = ","){
     if(base::nchar(sep) > 1)
         stop("argument 'sep' must be a length <= 1 character")
 
-    if(sep == ".")
-        stop("argument 'sep' must not be a '.' character")
+    if(sep == "." | sep == ";")
+        stop("use of ; or . forbidden for sep argument")
 
     #bugfix for character columns containing sep character
     colclasses <- as.character(lapply(cTable, class))
@@ -62,17 +64,20 @@ write2file <- function(object, file, sep = ","){
         stop("missing value detected in group column")
 
     group <- cTable[["group"]]
-
-    ##print the first line, plus column names
     write.table(cTable[1,], file = file, row.names = FALSE, sep = sep, na = "")
 
     if(!file.exists(file))
-        stop("invalid file- cannot write to indicated file")
+        stop("invalid file argument- cannot write to indicated file")
 
-    lines <- apply(cTable,1, function(row) base::paste(row, collapse = sep))
+    if(any(duplicated(names(cTable))))
+        lines <- do.call(paste, c(cTable, list(sep = sep)))
+    else
+        lines <- tidyr::unite(cTable, col = "dat", names(cTable), sep = ",")$dat
 
-    .Call("write2file", lines = lines, file = file, groups = group,
+    .Call("write2file", lines = lines, file = file, groups = group, code = 0,
             PACKAGE = "metabCombiner")
+
+    return()
 }
 
 
