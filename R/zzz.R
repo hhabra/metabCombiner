ae98b0c78aa9529037fbbe78a9a3c4021ad13bf4
Hani Habra <hhabra1@gmail.com>
2023-04-11 07:01:41 -0400

    start-up messages suppressed

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,15 @@
+
+.onLoad <- function(libname, pkgname) {
+    quietly <- getOption('quietly')
+    options(quietly = T)
+    suppressPackageStartupMessages(
+        requireNamespace("dplyr", quietly = TRUE)
+    )
+}
+
+
 .onUnload <- function(libpath) {
     library.dynam.unload("metabCombiner", libpath)
 }
+
+
